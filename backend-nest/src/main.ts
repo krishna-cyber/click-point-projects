c0bf316c5c2c58f392e9252ea4756ba82330dd77
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2025-08-26 11:40:25 +0545

    refactor: remove unused Console import from main.ts

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
-import { Console } from 'console';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
